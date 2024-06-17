995c5225418b5a4d2258e350b8e4cfcd88960c30
mohamed <algamalmohamed678@gmail.com>
2024-06-17 21:54:21 +0300

    add patient view

--- models/patient.py
+++ models/patient.py
@@ -2,7 +2,6 @@
 from odoo import api, fields, models, _
 from datetime import date
 from odoo.exceptions import ValidationError
-from dateutil import relativedelta
 
 
 class Patient(models.Model):
@@ -17,27 +16,36 @@ class Patient(models.Model):
     age = fields.Integer(string='Age', compute='_compute_age', store=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='gender')
     image = fields.Image(string='Image')
-    # patient_ = fields.Many2one('hospital.apporintent', string='Apportinent')
+    # patient_ = fields.Many2one('hospital.appointment', string='appointment')
     # appointment_count = fields.Integer(string='Appointment count', compute='_compute_appointment_count', store=True)
-    # appointment_ids = fields.One2many('hospital.apporintent', 'patient_id', string='Appointment')
-    # parent = fields.Char(string='Parent')
+    # appointment_ids = fields.One2many('hospital.appointment', 'patient_id', string='Appointment')
+    parent = fields.Char(string='Parent')
+    partner_name = fields.Char(string='Partner Name')
     marital_status = fields.Selection([
         ('single', 'Single'),
         ('married', 'Married'),
-        ('other', 'Other'),  # Consider including this for flexibility
+        ('other', 'Other'),
     ], string='Marital Status')
-    partner_name = fields.Char(string='Partner Name')
-    operation_id = fields.Many2one('hospital.operation', string='Operation')
+    operation_id = fields.Many2one('operation', string='Operation')
     is_birthday = fields.Boolean(string='Is Birthday ?', compute='_compute_is_birthday')
     phone = fields.Char(string="Phone")
     email = fields.Char(string="Email")
     website = fields.Char(string="Website")
 
-    @api.depends('date_of_birth')  # because age depends date_of_birth when add value in date_of_birth
+    @api.depends('date_of_birth')
     def _compute_age(self):
-        for rec in self:  # because don't make singleton error
+        for rec in self:
             today = date.today()
             if rec.date_of_birth:
                 rec.age = today.year - rec.date_of_birth.year
-            else:  # you should add else in compute function
+            else:
                 rec.age = 0
+
+    @api.depends('date_of_birth')
+    def _compute_is_birthday(self):
+        for rec in self:
+            rec.is_birthday = False
+            if rec.date_of_birth:
+                if date.today().day == rec.date_of_birth.day and date.today().month == rec.date_of_birth.month:
+                    rec.is_birthday = True
+
