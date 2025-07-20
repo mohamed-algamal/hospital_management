7402b2fdff80fc16ae72e65a7a2b22f4053c1960
mohamed-algamal <algamalmohamed678@gmail.com>
2025-07-20 16:00:36 +0300

    commint security

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'depends': ['base', 'mail',],
     'data': [
         'security/security.xml',
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'data/sequence_data.xml',
         'data/mail_template_appointment.xml',
         'data/mail_template_operation.xml',
